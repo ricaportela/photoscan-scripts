c02243dfbf07d3d8c7f965aa7072176a940bbb81
ricaportela <ricaportela@gmail.com>
2016-10-19 13:11:20 -0200

    Change Files

--- .drafts/myparserconfig.py
+++
@@ -1,8 +0,0 @@
-import configparser
-config = configparser.ConfigParser()
-config['DEFAULT'] = {'ProjectName':    'banquinho',
-                     'ChunckName':     'banquinho',
-                     'project_folder': '/home/ricardo/LearnPath/python/geoprocess-photoscan-cobolman/',
-                     'folder_images':  '/source_images/',
-                     'doc_name':       'banquinho',
-                     'images_pattern': '*.JPG'. 
